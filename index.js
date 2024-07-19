0a695be901ddb493fc6f857dfbdfa4ce49d035f1
merkulovva <medushevskaya.vv@students.dvfu.ru>
2024-07-19 21:03:00 +1000

    Add new functions to server

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 const http = require('http');
+const url = require('url');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello, Octagon!</h1>');
+    const parsedUrl = url.parse(req.url, true);
+    const pathname = parsedUrl.pathname;
+    const query = parsedUrl.query;
+
+    if (pathname === '/static') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ header: "Hello", body: "Octagon NodeJS Test" }));
+    } else if (pathname === '/dynamic') {
+        const a = parseFloat(query.a);
+        const b = parseFloat(query.b);
+        const c = parseFloat(query.c);
+
+        if (isNaN(a) || isNaN(b) || isNaN(c)) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ header: "Error" }));
+        } else {
+            const result = (a * b * c) / 3;
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ header: "Calculated", body: result.toString() }));
+        }
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<h1>Page Not Found</h1>');
+    }
 });
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
