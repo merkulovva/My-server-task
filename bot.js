ab45df3dbcfdb46d82197d90e0c95665b8d3e768
merkulovva <medushevskaya.vv@students.dvfu.ru>
2024-07-20 15:53:17 +1000

    bot menu added - 3 commands

--- bot.js
+++ bot.js
@@ -3,11 +3,32 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '7057608563:AAEotFzag53RI69MQGTCVtv2n-XBRfF-K3U';
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on('message', (msg) => {
+//start
+bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    const text = msg.text;
+    bot.sendMessage(chatId, 'Привет, октагон!');
+});
 
-    if (text === '/start') {
-        bot.sendMessage(chatId, 'Привет, октагон!');
-    }
+//help
+bot.onText(/\/help/, (msg) => {
+    const chatId = msg.chat.id;
+    const helpMessage = `
+    Список доступных команд:
+    /help - возвращает список команд, с их описанием
+    /site - отправляет в чат ссылку на сайт Октагона
+    /creator - отправляет в чат мое ФИО
+    `;
+    bot.sendMessage(chatId, helpMessage);
+});
+
+//site
+bot.onText(/\/site/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'https://students.forus.ru');
+});
+
+//creator
+bot.onText(/\/creator/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Медушевская Вероника Витальевна');
 });
