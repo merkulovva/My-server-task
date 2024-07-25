2f3c5bb44fd58efd717abd47992efa54c8d096d3
merkulovva <146440418+merkulovva@users.noreply.github.com>
2024-07-25 15:39:08 +1000

    change code

--- index.js
+++ index.js
@@ -4,38 +4,46 @@ const url = require('url');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const handleStatic = (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ header: "Hello", body: "Octagon NodeJS Test" }));
+};
+
+const handleDynamic = (req, res, query) => {
+    const params = ['a', 'b', 'c'];
+    const values = params.map(param => parseFloat(query[param]));
+    
+    if (values.some(value => isNaN(value))) {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ header: "Error" }));
+    } else {
+        const result = values.reduce((acc, curr) => acc * curr, 1) / 3;
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ header: "Calculated", body: result.toString() }));
+    }
+};
+
+const requestHandler = (req, res) => {
     const parsedUrl = url.parse(req.url, true);
     const pathname = parsedUrl.pathname;
     const query = parsedUrl.query;
 
     if (pathname === '/static') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ header: "Hello", body: "Octagon NodeJS Test" }));
+        handleStatic(req, res);
     } else if (pathname === '/dynamic') {
-        const a = parseFloat(query.a);
-        const b = parseFloat(query.b);
-        const c = parseFloat(query.c);
-
-        if (isNaN(a) || isNaN(b) || isNaN(c)) {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({ header: "Error" }));
-        } else {
-            const result = (a * b * c) / 3;
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({ header: "Calculated", body: result.toString() }));
-        }
+        handleDynamic(req, res, query);
     } else {
         res.statusCode = 404;
         res.setHeader('Content-Type', 'text/html');
         res.end('<h1>Page Not Found</h1>');
     }
-});
+};
+
+const server = http.createServer(requestHandler);
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
-
